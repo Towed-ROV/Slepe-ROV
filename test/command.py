f26c3a805408e74821da97c4f7bcc03e22c4b06f
UltraReidar <jorgenringda@gmail.com>
2021-02-11 16:39:19 +0100

    remove test

--- test/command.py
+++
@@ -1,5 +0,0 @@
-class Command:
-    def __init__(self, name, value):
-        self.command_name = name
-        self.command_value = value
-
--- test/test.py
+++
@@ -1,8 +0,0 @@
-stig = {}
-stig["kkk"] = 12
-stig["2312"] = 2321
-print(stig)
-stig["kkk"] =123
-print(stig)
-if "kkkk" in stig.keys():
-    print("ok")
